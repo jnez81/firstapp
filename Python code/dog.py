fe2758975e56dd30a288f7eaf7782ca170d4290f
James Martinez <jnez.81@gmail.com>
2020-12-01 10:12:33 -0700

    added get_color method for dog's color

--- Python code/dog.py
+++ Python code/dog.py
@@ -1,9 +1,10 @@
 # Object Oriented Programming Examples
 
 class Dog:
-    def __init__(self, name, age): 
+    def __init__(self, name, age, color): 
         self.name = name
         self.age = age
+        self.color = color 
 
     def get_name(self):
         return self.name
@@ -11,10 +12,14 @@ class Dog:
     def get_age(self):
         return self.age
 
+    def get_color(self):
+        return self.color
+
     def set_age(self, age):
         self.age = age
 
-d = Dog("James", 39)
+d = Dog("James", 39, "brown")
 print(d.get_name())
 print(d.get_age())
+print(d.get_color())
 
